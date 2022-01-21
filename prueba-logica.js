b487a5ecf17cca1543279f875f7bd8d9f8dc3ba9
Adriana Pleitez <40505528+adripleitez@users.noreply.github.com>
2022-01-21 07:45:14 -0600

    Adding validation of ascending order

--- prueba-logica.js
+++ prueba-logica.js
@@ -3,26 +3,29 @@ cars = ["UberX", "UberXL", "UberPlus", "UberBlack", "UberSUV"];
 discount = 20;
 
 I = prompt("Please enter the duration of your trip in miles:");
-console.log("Please enter the cost of a mile for each car: 'UberX', 'UberXL', 'UberPlus', 'UberBlack' and 'UberSUV'\n");
 for (var i = 0; i < 5; i++) {
-    fares[i] = prompt(cars[i] + ": ");
+    fares[i] = prompt("Enter the cost of a mile for " + cars[i] + ": ");
 }
 
 fancyRide(I, fares);
-
-// Example
-// console.log(fancyRide(30,[0.3,0.5,0.7,1,1.3]));
 
 function fancyRide(I, fares) {
     if (I < 4 || I > 30)
         return "The miles cannot be less than 4 or more than 30 miles.";
 
     if (fares.every(e => { return e >= 0.3 && e <= 5.0 })) {
-        for (let i = fares.length-1; i >= 0 ; i--) {
-            if( (I * fares[i]) <= discount){
-                return cars[i];
-            }   
+        if(fares.filter((f, i) => f > fares[i + 1]).length === 0){
+            for (let i = fares.length-1; i >= 0 ; i--) {
+                if( (I * fares[i]) <= discount){
+                    return cars[i];
+                }   
+            }
+        }else{
+            return "Mileage cost must be in ascending order.";
         }
     }else
         return "Mileage cost cannot be less than 0.3 or more than 5.0.";
 }
+
+// Example
+// console.log(fancyRide(30,[0.3,0.5,0.7,1,1.3]));
